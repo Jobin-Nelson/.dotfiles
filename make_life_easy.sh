116be0491899e0d900a0f3f3acbcbb35808b4751
Jobin Nelson <jobinnelson369@gmail.com>
2022-12-25 12:36:42 +0530

    added packages for pyenv in make_life_easy.sh

--- make_life_easy.sh
+++ make_life_easy.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
 # Install packages
-echo "Installing packages..."
-sudo apt-get -y install curl build-essential libfuse2 unzip git tmux zoxide ripgrep fzf 
+echo -e '\nInstalling packages...\n'
+sudo apt-get -y install \
+    git \
+    curl \
+    libfuse2 \
+    build-essential \
+    zlib1g-dev \
+    libffi-dev \
+    libssl-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    liblzma-dev \
+    tk-dev \
+    unzip \
+    tmux \
+    zoxide \
+    ripgrep \
+    fzf 
 
 # Installing Pyenv
+echo -e '\nInstalling pyenv...\n'
 curl https://pyenv.run | bash
 
 # Installing NVM
+echo -e '\nInstalling nvm...\n'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # Install Rustup
+echo -e '\nInstalling rustup...\n'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Setting up Pyenv, NVM, Rustup
+# Starship prompt
+echo -e '\nInstalling up starship prompt...\n'
+curl -sS "https://starship.rs/install.sh" | sh
+
+# Setting up pyenv, nvm, rust-analyzer
+echo -e '\nSourcing .bashrc...\n'
 source ~/.bashrc
 
 pyenv update
-pyenv install 3.11.1
-pyenv global 3.11.1
+echo -e '\nInstalling python...\n'
+pyenv install 3.11.1 && pyenv global 3.11.1
 
-nvm install node
-nvm use node
+echo -e '\nInstalling node...\n'
+nvm install node && nvm use node
 
+echo -e '\nInstalling rust-analyzer...\n'
 mkdir -p ~/playground/open_source/ && cd "$_" || exit 1
 git clone --depth 1 https://github.com/rust-lang/rust-analyzer.git && cd rust-analyzer || exit 1
 cargo xtask install --server
@@ -30,10 +56,6 @@ cargo xtask install --server
 # Configuring pop-os settings
 # echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode
 # gsettings set org.gnome.shell.app-switcher current-workspace-only true
-
-# Starship prompt
-echo "Setting up starship prompt..."
-curl -sS "https://starship.rs/install.sh" | sh
 
 echo "Done!!!"
 exit 0 
