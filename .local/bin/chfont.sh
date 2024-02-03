edbc4d00d786bc09f507568110b1b7bcf36f5ef7
Jobin Nelson <jobinnelson369@gmail.com>
2024-02-03 11:45:14 +0530

    feat: add kitty configuration

--- .local/bin/chfont.sh
+++ .local/bin/chfont.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 function get_font() {
-    declare -a FONTS
+  declare -a FONTS
 
-    FONTS=(
-        'JetBrainsMono Nerd Font'
-        'MesloLGS Nerd Font'
-        'Hack Nerd Font'
-        'SauceCodePro Nerd Font'
-        'Caskaydia Cove Nerd Font'
-        'Ubuntu Mono Nerd Font'
-        'FiraCode Nerd Font'
-        'Rec Mono Semicasual'
-        'Rec Mono Casual'
-        'Rec Mono Duotone'
-        'Rec Mono Linear'
-        'GeistMono Nerd Font'
-        'RobotoMono Nerd Font'
-    )
+  FONTS=(
+    'JetBrainsMono Nerd Font'
+    'MesloLGS Nerd Font'
+    'Hack Nerd Font'
+    'SauceCodePro Nerd Font'
+    'Caskaydia Cove Nerd Font'
+    'Ubuntu Mono Nerd Font'
+    'FiraCode Nerd Font'
+    'Rec Mono Semicasual'
+    'Rec Mono Casual'
+    'Rec Mono Duotone'
+    'Rec Mono Linear'
+    'GeistMono Nerd Font'
+    'RobotoMono Nerd Font'
+  )
 
-    choice=$(printf '%s\n' "${FONTS[@]}" \
-        | fzf --prompt 'Edit font: ' --layout=reverse --height='50%' --border --ansi)
+  choice=$(printf '%s\n' "${FONTS[@]}" \
+    | fzf --prompt 'Edit font: ' --layout=reverse --height='50%' --border --ansi)
 
-    echo "${choice}"
+  echo "${choice}"
 }
 
 function set_font() {
-    sed -i "s/^family = .*/family = \"$font\"/" "${ALACRITTY_FILE}"
+  sed -i "s/^family = .*/family = \"$font\"/" "${ALACRITTY_FILE}"
 
     # sed -Ei "
     # s/set [$]font '.*'/set \$font '${font}'/
@@ -34,45 +34,53 @@ function set_font() {
     # " "${I3_FILE}"
 
     echo "Font changed to $font"
-}
+  }
 
-function set_style() {
+  function set_style() {
     if [[ $font == 'JetBrainsMono Nerd Font' ]]; then
-        sed -zi '
-        s/style = "[^"]*"/style = "Medium Italic"/3
-        s/style = "[^"]*"/style = "Medium"/4
-        ' "${ALACRITTY_FILE}"
-        return
+      sed -zi '
+      s/style = "[^"]*"/style = "Medium Italic"/3
+      s/style = "[^"]*"/style = "Medium"/4
+      ' "${ALACRITTY_FILE}"
+      return
     fi
     sed -zi '
     s/style = "[^"]*"/style = "Italic"/3
     s/style = "[^"]*"/style = "Regular"/4
     ' "${ALACRITTY_FILE}"
-}
+  }
 
-function set_size() {
+  function set_size() {
     case "$font" in
-        'Ubuntu Mono Nerd Font')       sed -i 's/^size .*/size = 12/' "${ALACRITTY_FILE}" ;;
-        'JetBrainsMono Nerd Font')     sed -i 's/^size .*/size = 10/' "${ALACRITTY_FILE}" ;;
-        'SauceCodePro Nerd Font')      sed -i 's/^size .*/size = 11.5/' "${ALACRITTY_FILE}" ;;
-        *)                             sed -i 's/^size .*/size = 10.5/' "${ALACRITTY_FILE}" ;;
+      'Ubuntu Mono Nerd Font')       sed -i 's/^size .*/size = 12/' "${ALACRITTY_FILE}" ;;
+      'JetBrainsMono Nerd Font')     sed -i 's/^size .*/size = 10/' "${ALACRITTY_FILE}" ;;
+      'SauceCodePro Nerd Font')      sed -i 's/^size .*/size = 11.5/' "${ALACRITTY_FILE}" ;;
+      *)                             sed -i 's/^size .*/size = 10.5/' "${ALACRITTY_FILE}" ;;
     esac
-}
+  }
 
-function main() {
+  function set_kitty_font() {
+    sed -i "s/^\(font_family\s*\).*/\1${font}/1" "${KITTY_FILE}"
+    echo "Font changed to $font"
+  }
+
+  function main() {
     local ALACRITTY_FILE I3_FILE font
 
     ALACRITTY_FILE="$HOME/.config/alacritty/alacritty.toml"
     I3_FILE="$HOME/.config/i3/config"
+    KITTY_FILE="$HOME/.config/kitty/kitty.conf"
 
     font=$(get_font)
 
     [[ -z $font ]] && { echo "None selected. Aborting!"; exit 1; }
 
-    set_font 
-    set_style 
-    set_size
-}
+    [[ $TERM == 'xterm-kitty' ]] && set_kitty_font && return
 
-main
+    set_font
+    set_style
+    set_size
+  }
+
+  main
 
