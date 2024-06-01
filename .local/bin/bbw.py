c1b3b94adfae12c13035c74fad4d43fadc83489e
Jobin Nelson <jobinnelson369@gmail.com>
2024-06-01 18:05:39 +0530

    chore: bbw.py backup commit

--- .local/bin/bbw.py
+++ .local/bin/bbw.py
@@ -17,8 +17,6 @@ import asyncio
 async def exec_cmd(cmd: list[str]) -> bool:
     proc = await asyncio.create_subprocess_exec(
         *cmd,
-        stdout=asyncio.subprocess.DEVNULL,
-        stderr=asyncio.subprocess.DEVNULL,
     )
     await proc.wait()
     return proc.returncode == 0
@@ -56,7 +54,7 @@ async def git_status(cmd: list[str]):
     )
 
     stdout = await proc.stdout.read(1)
-    print(f'{cmd=}: {stdout=}')
+    # print(f'{cmd=}: {stdout=}')
     # print(f'{cmd[2]=}: {stderr=}')
     # if stdout == b'': return
     if stdout: print(f'{cmd[2]} repo is dirty')
