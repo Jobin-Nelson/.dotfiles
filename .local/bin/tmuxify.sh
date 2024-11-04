f572df3e9ded734c0bd9e96c6e8ef20d5121d52e
jnelson4 <jnelson4@illumina.com>
2024-11-04 14:14:33 +0530

    fix: replace . with _ creating tmux sessions in tmuxify.sh

--- .local/bin/tmuxify.sh
+++ .local/bin/tmuxify.sh
@@ -28,6 +28,7 @@ function sessionizer() {
   [[ -z $selected_dir ]] && exit 1
 
   session_name="${selected_dir##*/}"
+  session_name="${session_name//./_}"
 
   if ! tmux has-session -t "${session_name}" &> /dev/null; then
     tmux new-session -s "${session_name}" -c "${selected_dir}" -n 'editor' -d
