208cab07eca66b5c6558ef1f1f92f37638762359
jobin_nelson <jobinnelson369@gmail.com>
2025-03-02 10:18:49 +0530

    chore(nvim): update commit

--- .config/fzf/fzf-comp.sh
+++ .config/fzf/fzf-comp.sh
@@ -11,6 +11,7 @@
 
 [[ $0 = - ]] && return
 
+# password store
 _fzf_complete_pass() {
   _fzf_complete -- "$@" < <(pass git ls-files | sed \
     -e '/.gitattributes/d' \
@@ -23,4 +24,11 @@ _fzf_complete_pass() {
 #   post processing commands ...
 # }
 
-[ -n "$BASH" ] && complete -F _fzf_complete_pass -o default -o bashdefault pass
+[[ -n "$BASH" ]] && complete -F _fzf_complete_pass -o default -o bashdefault pass
+
+# tmux
+_fzf_complete_tmux() {
+  _fzf_complete --no-multi --ansi -- "$@" < <(tmux list-sessions -F '#{session_name}')
+}
+
+[[ -n "$BASH" ]] && complete -F _fzf_complete_tmux -o default -o bashdefault tmux
--- .local/bin/tmuxify.sh
+++ .local/bin/tmuxify.sh
@@ -13,14 +13,6 @@ trap cleanup SIGINT SIGTERM ERR EXIT
 
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                    Global Variables                      ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-
-
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                    Utility Functions                     ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
@@ -61,6 +53,7 @@ setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
     NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
   else
+    # shellcheck disable=SC2034
     NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
   fi
 }
@@ -195,7 +188,6 @@ kill_session() {
 parse_params() {
   # default values of variables set from params
   [[ -n ${TMUX-} ]] && fzf_height='100%' || fzf_height='50%'
-  cmd=''
 
   while :; do
     case "${1-}" in
