b518270f2ab729aaadcc86f1d894d537421ec93d
jobin_nelson <jobinnelson369@gmail.com>
2025-04-07 22:50:04 +0530

    feat(weekly_job): update paths for projects

--- .local/bin/weekly_jobs.sh
+++ .local/bin/weekly_jobs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 function git_push() {
-  local git_dir
-
-  git_dir=$1
+  local -r git_dir=$1
 
   cd "${git_dir}" \
     && git add -A \
@@ -12,19 +10,15 @@ function git_push() {
 }
 
 function main() {
-  local PROJECT_DIR
-  local -a projects
-
-  PROJECT_DIR="$HOME/playground/projects"
-
-  projects=(
-    'learn'
-    'org_files'
-    'second_brain'
+  local -a projects=(
+    "$HOME/playground/projects/learn"
+    "$HOME/playground/org_files"
+    "$HOME/playground/second_brain"
+    "$HOME/.password-store"
   )
 
   for project in "${projects[@]}"; do
-    git_push "${PROJECT_DIR}/${project}"
+    git_push "${project}"
   done
 }
 
